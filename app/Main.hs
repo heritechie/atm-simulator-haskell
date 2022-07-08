7671e03b6d37a8e20a6971dd750d5f5702809336
hery <heriyanto@fazzfinancial.com>
2022-07-08 08:17:11 +0700

    change AuthState data type

--- app/Main.hs
+++ app/Main.hs
@@ -1,32 +1,50 @@
+module Main where
+
 import Data.Time
 import qualified Data.Text as T
-import Control.Monad.Trans.State (State, execState, modify, get)
+import Control.Monad.Trans.State
+import Control.Monad.IO.Class
 
 main :: IO()
 main = do
     isAuthenticate <- readAuthPhoneNumber
     if not (fst isAuthenticate) then do
         putStrLn "\nAccount is not found\n"
+        putStrLn "[hint] Select phone number from db/accounts.csv\n"
         putStrLn "(Enter) Retry\n"
         _ <- getLine
         main
     else do
-        -- let authUser = snd isAuthenticate
-        -- print (execState createSession authUser)
-        menuSelector
+        let authUser = snd isAuthenticate
+        case authUser of
+            Just a ->  do
+                menuSelector
+            Nothing -> main
+    
 
--- createSession :: State AuthState ()
--- createSession = do
---    modify (authState)
+getAuthUser :: State AuthState (AuthState)
+getAuthUser = do
+    authUser <- get
+    return authUser
 
-
-phoneExist :: String -> [[String]] -> Bool
-phoneExist a xss = any (elem a) xss
+findIdxFromPhone :: String -> [[String]] -> Maybe Int
+findIdxFromPhone a xss = do 
+    let exist = any (elem a) xss
+    let foundIdx = 0 -- TODO: find from nested list
+    if exist then do 
+        return foundIdx
+    else Nothing
 
 splitToText :: String -> [T.Text]
 splitToText a = T.splitOn (T.pack ";") (T.pack a)
 
-data AuthState = AuthState Int String Int deriving (Show) 
+data AuthState = AuthState 
+    {
+        uid :: String,
+        phoneNumber :: String,
+        accountNo :: String,
+        balance :: String
+    } deriving (Show) 
 
 readAuthPhoneNumber :: IO ((Bool, Maybe AuthState))
 readAuthPhoneNumber = do
@@ -37,10 +55,21 @@ readAuthPhoneNumber = do
             contents <- readFile "db/accounts.csv"  
             let singleLine = tail $ lines contents
             let accounts = map (\a -> map (\b -> T.unpack b) (splitToText a)) singleLine
-            let isAuthenticated = phoneExist authPhone accounts
-            let authUser = AuthState 1 "2204050000001" 10000000
-            return (isAuthenticated, Just authUser)
+            let maybeIdx = findIdxFromPhone authPhone accounts
+            case maybeIdx of
+                Just a ->  do
+                        let account = accounts !! a
+                        let authUser = AuthState {
+                            uid=account !! 0, 
+                            phoneNumber=account !! 4,
+                            accountNo=account !! 2,
+                            balance=account !! 5}
+                        let _ = execState getAuthUser authUser
+                        return (True, Just authUser)
+                Nothing -> do
+                    return (False, Nothing)
     else return (False, Nothing)
+
 
 transferConfirmation :: IO (Maybe String)
 transferConfirmation = do
@@ -70,8 +99,10 @@ getActionName a
 
 logAction :: String -> IO ()
 logAction action = do
+    let authUser = getAuthUser
+    -- print $ snd authUser
     currentTime <- getLocalCurrentDateTime
-    let logMsg = (show currentTime) ++ " | " ++ (getActionName action) ++ "\n"
+    let logMsg = (show currentTime) ++ " | " ++ "[USER] " ++ " " ++ (getActionName action) ++ "\n"
     appendFile "logs.txt" logMsg
 
 getLocalCurrentDateTime :: IO LocalTime
